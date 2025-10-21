83eff1f443078ff231706e18fe1add943e43cb29
Hugo Herrador <hherrador@indra.es>
2025-10-21 12:29:03 +0200

    use geojsonlayer from deckgl to read from wfs

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import './style.css'
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
+//import MilSymbol from 'milsymbol';
+import { MapboxOverlay } from '@deck.gl/mapbox';
+import { ScatterplotLayer, GeoJsonLayer } from '@deck.gl/layers';
+//const sourceId = 'entity-source';
+//const layerId = 'entity-layer';
 
 const style = {
   "version": 8,
@@ -25,7 +30,7 @@ const style = {
 
 const map = new maplibregl.Map({
   container: 'map',
-  style: 'https://demotiles.maplibre.org/style.json' /*style*/,
+  style: style,
   center: [-3.97300533, 40.79907993,],
   zoom: 5
 });
@@ -39,4 +44,31 @@ map.addControl(new maplibregl.NavigationControl({
 
 map.on('zoomend', () => {
   console.log('Current zoom level:', map.getZoom());
+})
+
+map.on('load', () => {
+  const geoJsonDataUrl = `http://localhost:8081/geoserver/ows?service=WFS&version=1.1.0&request=GetFeature&typename=testing_the_waters:spain_osm_postcode_points&outputformat=application/json&srsName=EPSG:4326`;
+  
+  //ESTO SERÍA PARA SI LAS FEATURES HAY QUE MONTARLAS O LEERLAS DE ALGUN LADO
+  // const scatter = ScatterplotLayer({
+  //   id: "postal-codes",
+  //   data: features,
+  //   getPosition: d => d.geometry.coordinates,
+  //   getFillColor: [0, 150, 255],
+  //   getRadius: 1,
+  //   radiusUnits: "pixels",
+  //   pickable: false
+  // });
+const scatter = new GeoJsonLayer({
+    id: "geojson-points",
+    data: geoJsonDataUrl, // Deck.gl fetches and parses this GeoJSON FeatureCollection
+    pointType: "circle",
+    getPointRadius: 1,
+    getFillColor: [0, 150, 255],
+    pointRadiusUnits: "pixels",
+    pointRadiusMinPixels: 1,
+    pickable: false
+});
+  const overlay = new MapboxOverlay({ layers: [scatter], interleaved: false });
+  map.addControl(overlay);
 })
